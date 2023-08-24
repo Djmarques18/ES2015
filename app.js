6f6e5c5eb233fca017fcdd25d923a457f55677e2
Derrick Marques <127461231+Djmarques18@users.noreply.github.com>
2023-08-24 17:34:12 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const PI = 3.14;
+PI = 42;
+
+
+// The main difference between var and let in JavaScript is that var is function-scoped and subject to variable hoisting, while let is block-scoped and doesn't have hoisting-related issues.
+
